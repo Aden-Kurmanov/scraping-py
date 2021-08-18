f5bf6cc86968001125a063582608136bafed92be
Aden <aden.kurmanov@mail.ru>
2021-08-18 23:44:25 +0600

    lesson 1, task 1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from pprint import pprint
 import json
-
+# TODO ПРОШУ РАЗРЕШИТЬ ОТПРАВИТЬ ВТОРОЕ ЗАДАНИЕ НА ВЫХОДНЫХ
 user_agent_value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
 
 url = 'https://api.github.com/users/Aden-Kurmanov/repos'
