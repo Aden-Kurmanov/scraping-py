3851330d7b129d4881f6e204f461749141d57ae7
Aden <aden.kurmanov@mail.ru>
2021-08-21 14:50:40 +0600

    lesson 2

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
+from bs4 import BeautifulSoup as bs
 import requests
-from pprint import pprint
-import json
-# TODO ПРОШУ РАЗРЕШИТЬ ОТПРАВИТЬ ВТОРОЕ ЗАДАНИЕ НА ВЫХОДНЫХ
-user_agent_value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
+from pprint import pprint as p
 
-url = 'https://api.github.com/users/Aden-Kurmanov/repos'
+search_position = input('Введите должность: ')
+search_pages = input('На скольких страницах искать: ')
 
-headers = {
-    "User-Agent": user_agent_value,
-    "Accept": "application/vnd.github.v3+json",
-}
+is_not_num = True
+while is_not_num:
+    try:
+        search_pages = int(search_pages)
+        is_not_num = False
+    except:
+        print()
+        print('Требуется ввести число!')
+        search_pages = input('На скольких страницах искать: ')
 
-response = requests.get(url, headers=headers)
 
-json_res = response.json()
+    for i in range(search_pages):
+        print('index: ', i)
+        url = 'https://hh.ru/'
+        params = {
+            'area': '159',
+            'fromSearchLine': 'true',
+            'st': 'searchVacancy',
+            'text': search_position
+        }
 
-pprint(json_res)
+        if i > 0:
+            params['page'] = i
 
-print()
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+        }
 
-lst = list()
+        response = requests.get(f"{url}/search/vacancy", params=params, headers=headers)
 
-for it in json_res:
-    lst.append(it.get("full_name"))
-
-print("lst: ", lst)
-
-with open("repos.json", 'w') as file:
-    json.dump(lst, file)
+        soup = bs(response.text, 'html.parser')
+        blocks = soup.find('div', attrs={'class': 'bloko-gap'})
+        print(blocks)
