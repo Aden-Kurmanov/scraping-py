1fb51eca4ebb39c341584b8538b367a2119faa70
Aden <aden.kurmanov@mail.ru>
2021-08-21 21:12:06 +0600

    lesson 1 add task 2

--- main-2.py
+++ main-2.py
@@ -1,29 +1,24 @@
 import requests
-from pprint import pprint
+from pprint import pprint as p
 import json
+
 user_agent_value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
 
-url = 'https://api.github.com/users/Aden-Kurmanov/repos'
+url = 'https://api.github.com/user'
+
+token = ""
 
 headers = {
     "User-Agent": user_agent_value,
     "Accept": "application/vnd.github.v3+json",
+    "Authorization": f'token {token}'
 }
 
 response = requests.get(url, headers=headers)
 
 json_res = response.json()
 
-pprint(json_res)
+p(json_res)
 
-print()
-
-lst = list()
-
-for it in json_res:
-    lst.append(it.get("full_name"))
-
-print("lst: ", lst)
-
-with open("repos.json", 'w') as file:
-    json.dump(lst, file)
+with open('auth.json', 'w') as file:
+    json.dump(json_res, file)
