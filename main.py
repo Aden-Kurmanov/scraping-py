8c06c68e06613fe72de658db29a7ca4f340d5328
Aden <aden.kurmanov@mail.ru>
2021-08-21 19:55:58 +0600

    lesson 2 fix

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ while is_not_num:
         print('Требуется ввести число!')
         search_pages = input('На скольких страницах искать: ')
 
-url = 'https://www.superjob.ru/'
+url = 'https://www.superjob.ru'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
@@ -56,7 +56,9 @@ for i in range(search_pages):
     for block in blocks:
         obj_data = {}
         link_salary_block = block.find('div', attrs={'class', 'jNMYr GPKTZ _1tH7S'})
-        obj_data['name'] = link_salary_block.find('a', attrs={'class', 'icMQ_'}).getText()
+        a_block = link_salary_block.find('a', attrs={'class', 'icMQ_'})
+        obj_data['name'] = a_block.getText()
+        obj_data['link'] = url + a_block['href']
         main_salary_block = link_salary_block.find('span', attrs={'class', 'f-test-text-company-item-salary'})
         # print(obj_data['name'])
         if len(main_salary_block.findChildren(recursive=False)) == 1:
@@ -91,115 +93,9 @@ for i in range(search_pages):
                         obj_data['min_salary'] = int(salary_list[0])
                         obj_data['max_salary'] = int(salary_list[0])
                         obj_data['currency'] = salary_list[len(salary_list) - 1]
-            # len_s = len(salary_block.findChildren(recursive=False))
-            # if len_s == 0:
-            #     min_s = salary_block.getText()
-            #     print(obj_data['name'])
-            #     print(min_s)
-            #     obj_data['min_salary'] = ""
-            #     obj_data['max_salary'] = None
-            #     obj_data['currency'] = ""
-            # elif len_s == 1:
-            #     obj_data['min_salary'] = ""
-            #     obj_data['max_salary'] = ""
-            #     obj_data['currency'] = ""
 
         print('')
-        # if salary_block.children == 0:
-        #     print('salary chat: ', salary_block.getText())
-
-        # obj_data['min_salary'] = link_salary_block.find('span', attrs={'class', '_1h3Zg'}).getText()
         jobs_list.append(obj_data)
 
 p(jobs_list)
-
-# for i in range(search_pages):
-#     print('index: ', i)
-#     url = 'https://hh.ru/'
-#     params = {
-#         'area': '159',
-#         'fromSearchLine': 'true',
-#         'st': 'searchVacancy',
-#         'text': search_position
-#     }
-#
-#     if i > 0:
-#         params['page'] = i
-#
-#     # headers = {
-#     #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
-#     # }
-#     # full_url = f"{url}/search/vacancy"
-#     full_url = f"{url}/search/vacancy?area=159&fromSearchLine=true&st=searchVacancy&text={search_position}"
-#
-#     if i > 0:
-#         full_url += f'&page={i}'
-#
-#     options = webdriver.ChromeOptions()
-#     options.add_argument('User-Agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"')
-#     browser = webdriver.Chrome(options=options)
-#     browser.get(full_url)
-#     soup = bs(browser.page_source, 'html.parser')
-#     blocks = soup.find_all('div', attrs={'class': 'vacancy-serp-item'})
-#     print(blocks)
-#     browser.close()
-
-# for i in range(search_pages):
-#     print('index: ', i)
-#     url = 'https://hh.ru/'
-#     params = {
-#         'area': '159',
-#         'fromSearchLine': 'true',
-#         'st': 'searchVacancy',
-#         'text': search_position
-#     }
-#
-#     if i > 0:
-#         params['page'] = i
-#
-#     headers = {
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
-#     }
-#
-#     response = requests.get(f"{url}/search/vacancy", params=params, headers=headers)
-#
-#     soup = bs(response.text, 'html.parser')
-#     blocks = soup.find('div', attrs={'class': 'vacancy-serp'})
-#     print(blocks)
-
-
-
-
-# for i in range(search_pages):
-#     print('index: ', i)
-#     url = 'https://hh.ru/'
-#     params = {
-#         'area': '159',
-#         'fromSearchLine': 'true',
-#         'st': 'searchVacancy',
-#         'text': search_position
-#     }
-#
-#     if i > 0:
-#         params['page'] = i
-#
-#     headers = {
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
-#     }
-#     full_url = f"{url}/search/vacancy"
-#     # full_url = f"{url}/search/vacancy?area=159&fromSearchLine=true&st=searchVacancy&text={search_position}"
-#
-#     session = HTMLSession()
-#
-#     session.headers = headers
-#     session.params = params
-#
-#     resp = session.get(full_url)
-#     resp.html.render()
-#     resp.close()
-#
-#     soup = bs(resp.text, 'html.parser')
-#     blocks = soup.find_all('div', attrs={'class': 'vacancy-serp-item'})
-#     print(blocks)
-
 
