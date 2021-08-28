7c38140d96cdb38d13a247634063220f9af6d32b
Aden <aden.kurmanov@mail.ru>
2021-08-28 21:21:34 +0600

    lesson-4 lenta.ru

--- main.py
+++ main.py
@@ -1,128 +1,55 @@
-from bs4 import BeautifulSoup as bs
-from pymongo import MongoClient
-from pprint import pprint as p
+from lxml import html
 import requests
-
-search_position = input('Введите должность: ')
-search_pages = input('На скольких страницах искать: ')
-
+from pprint import pprint as p
+from pymongo import MongoClient
 
 client = MongoClient("127.0.0.1", 27017)
-
 db = client['geekBrains_data_from_net']
+news = db.news
 
-jobs = db.jobs
-
-is_not_num = True
-while is_not_num:
-    try:
-        search_pages = int(search_pages)
-        is_not_num = False
-    except:
-        print()
-        print('Требуется ввести число!')
-        search_pages = input('На скольких страницах искать: ')
-
-url = 'https://www.superjob.ru'
+url = 'https://lenta.ru/'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
 }
 
+response = requests.get(url, headers=headers)
 
-def get_salary_currency(string):
-    m = ''
-    c = ''
+dom = html.fromstring(response.text)
 
-    for s in string:
-        try:
-            s = int(s)
-            m += str(s)
-        except:
-            c += s
+top_section = dom.xpath("//section[contains(@class, 'js-top-seven')]")[0]
+top_main_news = top_section[0].xpath(".//div[@class='first-item']")[0]
+top_news = top_section.xpath(".//div[@class='item']")
 
-    return int(m), c
+top_main_news_data = {
+    'source': 'https://lenta.ru/',
+    'name': top_main_news.xpath(".//h2/a/text()")[0].replace('\xa0', ' '),
+    'link': url + top_main_news.xpath(".//a[contains(@class, 'topic-title-pic__link')]/@href")[0],
+    'date': top_main_news.xpath(".//time[@datetime]/@datetime")[0]
+}
 
+is_exist = False
+for _ in news.find({'link': top_main_news_data['link']}):
+    is_exist = True
+    break
+if not is_exist:
+    news.insert_one(top_main_news_data)
 
-for i in range(search_pages):
-    params = {
-        'keywords': search_position
+for item in top_news:
+    item_info = {
+        'source': 'https://lenta.ru/',
+        'name': item.xpath("./a/text()")[0].replace('\xa0', ' '),
+        'link': url + item.xpath("./a[@href]/@href")[0],
+        'date': item.xpath(".//time[@datetime]/@datetime")[0]
     }
 
-    if i > 0:
-        params['page'] = i
-
-    response = requests.get(f"{url}/vacancy/search/", params=params, headers=headers)
-
-    soup = bs(response.text, 'html.parser')
-    all_found = soup.find_all('div', attrs={'class': '_3zucV _3rq8C _1b_NL'})
-    if len(all_found) == 1:
+    is_exist = False
+    for _ in news.find({'link': item_info['link']}):
+        is_exist = True
         break
-    main = all_found[1]
-    blocks = main.find_all('div', attrs={'class', 'f-test-vacancy-item'})
 
-    for block in blocks:
-        obj_data = {}
-        link_salary_block = block.find('div', attrs={'class', 'jNMYr GPKTZ _1tH7S'})
-        a_block = link_salary_block.find('a', attrs={'class', 'icMQ_'})
-        obj_data['link'] = url + a_block['href']
-        is_exist = False
-        for db_item in jobs.find({'link': obj_data['link']}):
-            is_exist = True
-            break
-        if not is_exist:
-            obj_data['name'] = a_block.getText()
-            loc_block = block.find('span', attrs={'class', 'f-test-text-company-item-location'})
-            loc_children = loc_block.findChildren(recursive=False)
-            obj_data['address'] = str(loc_children[len(loc_children) - 1].getText()).replace('\xa0', ' ')
-            main_salary_block = link_salary_block.find('span', attrs={'class', 'f-test-text-company-item-salary'})
-            if len(main_salary_block.findChildren(recursive=False)) == 1:
-                obj_data['min_salary'] = 'По договоренности'
-                obj_data['max_salary'] = None
-                obj_data['currency'] = None
-            elif len(main_salary_block.findChildren(recursive=False)) == 2:
-                salary_block = main_salary_block.find('span', attrs={'class', '_1h3Zg'})
-                salary_list = [str(x).replace('\xa0', '').replace(' ', '') for x in list(salary_block.children)]
-                vilka = [x for x in salary_list if x.find('<span') != -1]
-                if len(vilka) > 0:
-                    v_list = [x for x in salary_list if x.find('<span') == -1]
-                    obj_data['currency'] = v_list[len(v_list) - 1]
-                    obj_data['min_salary'] = int(v_list[0])
-                    obj_data['max_salary'] = int(v_list[1])
-                else:
-                    is_from = len([x for x in salary_list if x == 'от']) > 0
-                    if is_from:
-                        min_s, currency = get_salary_currency(str(salary_list[len(salary_list) - 1]))
+    if not is_exist:
+        news.insert_one(item_info)
 
-                        obj_data['min_salary'] = min_s
-                        obj_data['max_salary'] = None
-                        obj_data['currency'] = currency
-                    else:
-                        is_to = len([x for x in salary_list if x == 'до']) > 0
-                        if is_to:
-                            max_s, currency = get_salary_currency(str(salary_list[len(salary_list) - 1]))
-                            obj_data['min_salary'] = None
-                            obj_data['max_salary'] = max_s
-                            obj_data['currency'] = currency
-                        else:
-                            obj_data['min_salary'] = int(salary_list[0])
-                            obj_data['max_salary'] = int(salary_list[0])
-                            obj_data['currency'] = salary_list[len(salary_list) - 1]
-
-            jobs.insert_one(obj_data)
-
-salary = input('Введите минимальную заработную плату: ')
-
-is_not_num = True
-while is_not_num:
-    try:
-        salary = int(salary)
-        is_not_num = False
-    except:
-        print()
-        print('Требуется ввести число!')
-        salary = input('Введите минимальную заработную плату: ')
-
-for found in jobs.find({'$or': [{'min_salary': {'$gte': salary}}, {'max_salary': {'$gte': salary}}]}):
-    p(found)
-
+for item in news.find({}):
+    p(item)
